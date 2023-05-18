d1bf35fdfdc4749fe7d947deaf4fa6f3ed8ddfa7
S-Lombardi <silo.lombardi@gmail.com>
2023-05-18 20:34:58 +0200

    impostata sezione "il tuo biglietto"

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,5 @@
 
-let pulsanteGenera = document.getElementById('genera');
-
+const pulsanteGenera = document.getElementById('genera');
 
 pulsanteGenera.addEventListener('click', function() { 
     //input km e anni utente
@@ -12,28 +11,28 @@ pulsanteGenera.addEventListener('click', function() {
 
     //CALCOLO PREZZO INTERO DEL BIGLIETTO
     let prezzo_intero = (km_utente * prezzo_al_chilometro);
+   
 
     //DICHIARIAZIONE VARIABILE PER LO SCONTO
     let scontoDaApplicare;
-    
+    let nomeBiglietto;
+
     //CONDIZIONE
     //SCONTO MINORENNI
     if (anni_utente == 2 ) {
-
+        nomeBiglietto = "Biglietto minori";       
         scontoDaApplicare = 0.20;
         console.log("sconto del 20%: " + scontoDaApplicare);
-
     }
     //SCONTO OVER 65
     else if (anni_utente == 3) {
-
+        nomeBiglietto = "Biglietto over 65";
         scontoDaApplicare = 0.40;
         console.log("sconto del 40%: " + scontoDaApplicare);
-
     }
     //SCONTO A 0
     else {
-
+        nomeBiglietto = "Biglietto standard"
         scontoDaApplicare = 0;
         console.log("sconto 0%: " + scontoDaApplicare);
     }
@@ -46,7 +45,15 @@ pulsanteGenera.addEventListener('click', function() {
     console.log("prezzo finale: " + prezzo_finale_fixed);
 
     //RISULTATO A VIDEO
-    document.getElementById("risultato").innerHTML = prezzo_finale_fixed;
+    document.getElementById("offerta").innerHTML = nomeBiglietto;
+    document.getElementById("risultato").innerHTML = prezzo_finale_fixed + " €";
     document.getElementById("nomeCognomeDef").innerHTML = document.getElementById("nomecognome").value;
-}); 
 
+    //Carrozza e Codice CP
+    let carrozzaRandom = Math.floor(Math.random() * 11);
+    document.getElementById("numeroCarrozza").innerHTML =  carrozzaRandom;
+
+    let codiceCpRandom = Math.floor(Math.random() * 100001);
+    document.getElementById("codiceCp").innerHTML =  codiceCpRandom;
+
+});
